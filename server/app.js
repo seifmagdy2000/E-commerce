086aeb4e9eeaa54086db47446364fcaeacafe78d
seifmagdy2000 <seifmagdy20000@gmail.com>
2025-03-02 17:14:00 +0200

    finished the product routes and functionalities

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 import "dotenv/config";
 import express from "express";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.route.js";
 import productsRouter from "./routes/products.route.js";
 import DB from "./config/DB.js";
-import cookieParser from "cookie-parser";
+import cartRouter from "./routes/cart.route.js";
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/products", productsRouter);
+app.use("/api/cart", productsRouter);
 
 app.listen(PORT, () => {
   DB();
